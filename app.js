bdeb81255a3ac9a0f0afd1da3da5ff86f5ff6306
Shreyash Daundkar <productiveappa@gmail.com>
2023-09-27 21:30:52 +0530

    Created server that return custom responce based on the url user hits

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('appa');
+    res.setHeader('Content-Type', 'text/html');
+    let text = req.url == '/home' ? 'Home page' : req.url == '/about' ? 'About us page' : req.url == '/node' ? 'My Project' : 'Server';
+    res.write(`<html><head><title>Responce</title></head><body><h1>Welcome to ${text}</h1></body></html>`);
+    res.end();
 });
 
 server.listen(4000);
