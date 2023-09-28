615fa1e9fe7ab4e8aa8730253764fb748a006e8f
Shreyash Daundkar <productiveappa@gmail.com>
2023-09-28 08:26:32 +0530

    Now our server aslo display file data on top of our form using readfilesync method in fs module

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const fs = require('fs');
 
 const server = http.createServer((req, res) => {
      res.setHeader('Content-Type', 'text/html');
-     if(req.url === '/') res.write(`<html><head><title>Responce</title></head><body><form action="/msg" method="post"><input type="text" name="text"><input type="submit" value="send"></form></body></html>`);
-     else if(req.url === '/msg' && req.method === 'POST') {
+     if(req.url === '/') {
+        const msg = fs.readFileSync('msg.txt');
+        res.write(`<html><head><title>Responce</title></head><body><h1>${msg}</h1><form action="/msg" method="post"><input type="text" name="text"><input type="submit" value="send"></form></body></html>`);
+     }else if(req.url === '/msg' && req.method === 'POST') {
         const body = [];
         req.on('data', chunk => body.push(chunk));
         req.on('end', () => fs.writeFileSync('msg.txt', Buffer.concat(body).toString().split('=')[1]));
         res.writeHead(302, {'Location' : '/'});
-     } else res.write(`<html><head><title>Responce</title></head><body><h1>Welcome to Server</h1></body></html>`);
+     }else res.write(`<html><head><title>Responce</title></head><body><h1>Welcome to Server</h1></body></html>`);
     res.end();
 });
 
