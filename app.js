8b16567c692b9fd6a7a69aff293d812c594c2af4
Shreyash Daundkar <productiveappa@gmail.com>
2023-09-28 08:07:18 +0530

    Created server that take form input and print in txt file using stream and buffer  and redirect our webpage back to form

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-    let text = req.url == '/home' ? 'Home page' : req.url == '/about' ? 'About us page' : req.url == '/node' ? 'My Project' : 'Server';
-    res.write(`<html><head><title>Responce</title></head><body><h1>Welcome to ${text}</h1></body></html>`);
+     res.setHeader('Content-Type', 'text/html');
+     if(req.url === '/') res.write(`<html><head><title>Responce</title></head><body><form action="/msg" method="post"><input type="text" name="text"><input type="submit" value="send"></form></body></html>`);
+     else if(req.url === '/msg' && req.method === 'POST') {
+        const body = [];
+        req.on('data', chunk => body.push(chunk));
+        req.on('end', () => fs.writeFileSync('msg.txt', Buffer.concat(body).toString().split('=')[1]));
+        res.writeHead(302, {'Location' : '/'});
+     } else res.write(`<html><head><title>Responce</title></head><body><h1>Welcome to Server</h1></body></html>`);
     res.end();
 });
 
